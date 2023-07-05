2437fd700dfaf8af425a4ca7141862a1afbd0f43
Hussein <husseinfaqih@gmail.com>
2023-07-05 22:05:38 +0200

    tasks stored locally with vues store and implement delete logic

--- src/components/TaskComponent.vue
+++ src/components/TaskComponent.vue
@@ -1,22 +1,47 @@
 <template>
   <div>
     <h3 >{{ task.text }}</h3>
-    <i class="fas fa-times"></i>
-    <i class="fas fa-pen"></i>
+    <i class="fas fa-times" @click="deleteTask(task.id)"></i>
+    <i class="fas fa-pen" ></i>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 
 export default {
   name: 'TaskComponent',
   props: {
     task: Object
   },
-  
+  data() {
+    return {
+      taskText: "", // Stores the task text in the input field
+    };
+  },
+  methods: {
+    ...mapActions(['deleteTask']),
+
+  }
 }
 </script>
 
-<style >
-
+<style scoped>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
--- src/store/modules/tasks.js
+++ src/store/modules/tasks.js
@@ -27,13 +27,31 @@ const actions = {
   addTask({ commit }, task) {
     commit('add_task', task);
   },
+  deleteTask({ commit }, id) {
+    commit('delete_task', id);
+  },
 };
 
 const mutations = {
+  loadTasks(state) {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+      state.tasks = tasks;
+    }
+  },
   add_task(state, task) {
     state.tasks.push(task);
+    saveTasksToLocalStorage(state.tasks);
+  },
+  delete_task(state, id) {
+    state.tasks = state.tasks.filter((task) => task.id !== id);
+    saveTasksToLocalStorage(state.tasks);
   },
 };
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 export default {
   state,
