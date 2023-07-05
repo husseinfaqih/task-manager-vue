9f92d7a5f16229bdba18f031eaed2c522ae42b7e
Hussein <husseinfaqih@gmail.com>
2023-07-05 21:44:01 +0200

    Add logic to addTask through vuex and apply styling

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,9 +1,9 @@
 <template>
-  <form >
+  <form class="add-form" @submit.prevent="addTaskAndClearFields">
 
-    <div >
+    <div class="form-control">
       <label>Task</label>
-      <input type="text" name="text" placeholder="Add Task" />
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
     </div>
     
     <input type="submit" value="Save Task" class="btn btn-block" />
@@ -11,13 +11,71 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 
 export default {
-  name: 'AddTask'
- ,
+  name: 'AddTask',
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    ...mapActions(['addTask']),
+
+    addTaskAndClearFields() {
+
+      const newTask = {
+        id: Math.floor(Math.random() * 100000),
+        text: this.text,
+      };
+
+      this.addTask(newTask);
+
+      this.clearFields();
+    },
+
+  
+    clearFields() {
+      this.text = "";
+    }
+  },
 };
 </script>
 
-<style >
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
 
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
 </style>
